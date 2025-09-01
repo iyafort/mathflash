21196fb3626660cca83fd52c39cd0110f933bdbe
iyafort <iya.ragay29@gmail.com>
2025-09-02 06:23:50 +0800

    Flutter Initiation Fix

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c813f183e580d9190135c1a594e699c6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "89ecd3896f1012283ad27da04c8e1637",
+"flutter_bootstrap.js": "a3d2202ab83acb69243cd69003d0f61f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
