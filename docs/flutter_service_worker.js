b06020dc57d5e6148942eda0f90704e5420cc03d
iyafort <iya.ragay29@gmail.com>
2025-09-03 09:10:17 +0800

    App Guide updates and added rectangular bg

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -28,14 +28,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c813f183e580d9190135c1a594e699c6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "14ec891d6e0ea92d5767b90cb8341f99",
+"flutter_bootstrap.js": "2d2dd97a412386a91bc5d40a09c18735",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "2a218e149d1041e589702cbca4a15468",
 "/": "2a218e149d1041e589702cbca4a15468",
-"main.dart.js": "aad20c87789b460d64af5b8bcc7b817b",
+"main.dart.js": "6b3cd8c9e9b069c3632eeb0186220c12",
 "manifest.json": "b24cb79dc2bc0c11fb3c67c283d39f05",
 "version.json": "29936ed77c5de769df5965dc42444418"};
 // The application shell files that are downloaded before a service worker can
