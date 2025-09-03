46468173dbcd201e701fe242647db89476232967
iyafort <iya.ragay29@gmail.com>
2025-09-04 06:05:54 +0800

    settings updates

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1449053053"
+    serviceWorkerVersion: "27712006"
   }
 });
